cb259e4f43d81525513a55b9801bfa0f99391de5
jhonatan <jhonatanlira@gmail.com>
2024-09-10 20:21:37 -0300

    Criação de Telas

---
+++ CriaçaoDeTelas/ProjectCriacaoDeTelas.dpr
@@ -0,0 +1,14 @@
+program ProjectCriacaoDeTelas;
+
+uses
+  Vcl.Forms,
+  UnitCriacaoDeTelas in 'UnitCriacaoDeTelas.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
